559b2579a70da8e0f5033380820fd035e6617700
cs-coding-exercises <37808365+cs-coding-exercises@users.noreply.github.com>
2018-04-16 11:25:23 -0400

    Update asmd.rb

--- asmd.rb
+++ asmd.rb
@@ -1,30 +1,34 @@
+# https://askubuntu.com/questions/870530/how-to-install-geckodriver-in-ubuntu
+#wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz
+# tar -xvzf geckodriver*
+# chmod +x geckodriver
+# sudo mv geckodriver /usr/local/bin/
+
 home_page = "http://www.cars.com"
 
 require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :firefox
-wait = Selenium::WebDriver::Wait.new(:timeout => 15)
+wait = Selenium::WebDriver::Wait.new(:timeout => 30)
 
 #Login Page ---------------------------------------------
-Given("I am on the homepage") do
+Given /^I am on the homepage$/ do
   driver.navigate.to  "http://www.cars.com" 
 end
 
-When("I click the user icon") do
-  sleep 10
-  
+When /^I click the user icon$/ do
   driver.find_element(:class_name, "global-nav__profile-trigger").click()
 end
 
-And ("then click the login link") do
+And /^then click the login link$/ do
   driver.find_element(:link_text, "Log In").click()
 end
 
-Then("I should be brought to the login page") do
+Then /^I should be brought to the login page$/ do
   expected_url = "https://www.cars.com/profile/secure/login/?continue=%2F"
   driver.current_url == expected_url
 end
 
-And ("the login page should contain text") do
+And /^the login page should contain text$/ do
   driver.page_source.include? "Log In with Email"
   driver.page_source.include? "Email"
   driver.page_source.include? "Password"
@@ -32,38 +36,40 @@ And ("the login page should contain text") do
 end
 
 #Terms of service Scenario --------------------------------
-Given("We navigate to the homepage") do
+Given /^We navigate to the homepage$/ do
   driver.navigate.to  "http://www.cars.com" 
 end
 
-When("I click the terms of service link") do 
+When /^I click the terms of service link$/ do 
   driver.find_element(:link_text, "Terms of Service" ).click()
 end
 
-Then("I should be brought to the Terms of Service page") do
+Then /^I should be brought to the Terms of Service page$/ do
   expected_url = "https://www.cars.com/about/terms/"
   driver.current_url == expected_url
 end
 
-Then("TOS page should contain text {string}") do |string|
+Then /^TOS page should contain text "([^\"]*)"$/ do |string|
   driver.page_source.include? string
 end
 
 
 #Privacy Page ---------------------------------------------
-Given("We navigate back to the homepage") do
+Given /^We navigate back to the homepage$/ do
   driver.navigate.to  "http://www.cars.com" 
 end
 
-When("I click the Privacy Statement link") do
+
+When /^I click the Privacy Statement link$/ do
   driver.find_element(:link_text, "Privacy Statement").click()
 end
 
-Then("I should be brought to the Privacy Statement page") do
+Then /^I should be brought to the Privacy Statement page$/ do
   expected_url = "https://www.cars.com/about/privacy/"
   driver.current_url == expected_url
 end
 
-Then("Privacy page should contain text {string}") do |string|
+Then /^Privacy page should contain text "([^\"]*)"$/ do |string|
   driver.page_source.include? string
+  driver.quit()
 end
